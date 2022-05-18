b81ac89250f1d949e20e1088cf4a3cc8315b5d9a
vinithkumartatipally <vinitkumar096@gmail.com>
2022-05-18 09:21:50 -0400

    plugins

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,11 +8,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:zeta_fashion_application/LoginPage.dart';
 
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const ZetaFashion());
+    await tester.pumpWidget(const ZetaFashionState());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
